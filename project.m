92b3873b6f8e2023be6ebbc9b0bc6139ae9fa347
nkusry <46381825+nkusry@users.noreply.github.com>
2026-08-27 20:20:35 +0800

    Add files via upload

--- project.m
+++ project.m
@@ -1,10 +1,3 @@
-%data loading
-UA=xlsread('statistics.xlsx','C2:C130');
-Pdata=xlsread('statistics.xlsx','D2:D130');
-UR=xlsread('statistics.xlsx','E2:E130');
-UB=xlsread('statistics.xlsx','F2:F130');
-TAU=xlsread('statistics.xlsx','G2:G130');
-Rdata=xlsread('statistics.xlsx','H2:H130');
 %formate the figures
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultlegendinterpreter','latex');
@@ -59,14 +52,31 @@ axis([1 129 0.2 0.8]);set(gca,'ytick',0.2:0.2:0.8);
 set(gca,'XTickLabel',1995:5:2020);set(gca,'YTickLabel',{'20%','40%','60%','80%'});
 legend('Effective Replacement Rate','Optimal Replacement Rate (proposed model)','Optimal Replacement Rate (LMS model)')
 
-%plot figure 4
+%plot figure 4a
+plot(R_new-R_old)
+axis([1 129 0 0.1]);set(gca,'ytick',0:0.02:0.1);
+set(gca,'XTickLabel',1995:5:2020);
+%plot figure 4b
+BC_new=5.08.*(0.49-1.18.*(R_new-0.42)).*(0.12-0.26.*(R_new-0.42));
+UA_new=UA+UA./(1-Rdata).*epsM.*(R_new-Rdata);
+UB_new=UB+UB./(1-Rdata).*epssp.*(R_new-Rdata);
+TAU_new=TAU-1.5.*TAU.*(1+TAU)./(1-UB)./(1-Rdata).*epssp.*(R_new-Rdata);
+ratio=(1./Pdata-1).*UB_new./UA_new;
+net=0.82-1.5.*TAU_new./UB_new;
+ela=(R_new-R_old)./ratio./net;
+plot(ela,'b-');hold on;plot(net,'b--');hold on;plot(ratio,'b:');
+axis([1 129 0 2.5]);set(gca,'ytick',0:0.5:2.5);
+set(gca,'XTickLabel',1995:5:2020);
+legend('labour-market spillover elasticity','marginal net cost of unemployment',' relative unemployment-stock weight of non-eligible workers')
+
+%plot figure 5
 scatter(Rdata,UR);
 hold on;plot(fit(Rdata,UR,'poly1'));
 xlabel('$R$');ylabel('$u$');
 axis([0.2 0.8 0.03 0.1]);
 legend('off');
 
-%plot figure 5
+%plot figure 6
 duadR=epsM.*UA./(1-Rdata);
 dubdR=epssp.*UB./(1-Rdata);
 dudR_new=Pdata.*duadR+(1-Pdata).*dubdR;
@@ -75,13 +85,13 @@ axis([1 129 -0.02 0.02]);set(gca,'ytick',-0.02:0.01:0.02);
 set(gca,'XTickLabel',1995:5:2020);
 legend('$du/dR$','$du_i/dR$','$du_n/dR$');
 
-%plot Figure 6
+%plot Figure 7
 scatter(Pdata,UR);
 hold on;plot(fit(Pdata,UR,'poly1'));
 xlabel('Coverage Rate');ylabel('Unemployment Rate');
 legend('off');
 
-%plot Figure 7a
+%plot Figure 8a
 Roptimal=RP(Pdata);
 Roptimal1=RP(Pdata-0.05);
 Roptimal2=RP(Pdata+0.05);
@@ -90,7 +100,7 @@ axis([1 129 0.3 0.7]);set(gca,'ytick',0.3:0.1:0.7);
 set(gca,'XTickLabel',1995:5:2020);set(gca,'YTickLabel',{'30%','40%','50%','60%','70%'});
 legend('Optimal R (p)','Optimal R (p-0.05)','Optimal R (p+0.05)');
 
-%plot Figure 7b
+%plot Figure 8b
 dRdp1=(RP(Pdata+0.01)-RP(Pdata))./0.01;
 dRdp2=(RP(Pdata)-RP(Pdata-0.01))./0.01;
 dRdp=(dRdp1+dRdp2)./2;
@@ -102,23 +112,23 @@ set(gca,'XTickLabel',1995:5:2020);set(gca,'YTickLabel',{'0%','3%','6%','9%','12%
 legend('unemployment rate (observed)','unemployment rate (lower optimal R \& higher p)','unemployment rate (higher optimal R \& lower p)');
 
 
-%plot Figure 8a
+%plot Figure 9a
 plot(TAU,'b-');
 axis([1 129 0.01 0.05]);set(gca,'ytick',0.01:0.01:0.05);
 set(gca,'XTickLabel',1995:5:2020);set(gca,'YTickLabel',{'1%','2%','3%','4%','5%'});
 
-%plot Figure 8b
+%plot Figure 9b
 plot(Rdata,'b-');
 axis([1 129 0.2 0.8]);set(gca,'ytick',0.2:0.2:0.8);
 set(gca,'XTickLabel',1995:5:2020);set(gca,'YTickLabel',{'20%','40%','60%','80%'});
 
-%plot Figure 9a
+%plot Figure 10a
 Recipiency=xlsread('statistics.xlsx','I2:I130');
 plot(Recipiency,'b-');
 axis([1 129 0 1]);set(gca,'ytick',0:0.2:1);
 set(gca,'XTickLabel',1995:5:2020);set(gca,'YTickLabel',{'0%','20%','40%','60%','80%','100%'});
 
-%plot Figure 9b
+%plot Figure 10b
 LongUnemRatio=xlsread('statistics_m.xlsx','F2:F388');
 plot(quarter(LongUnemRatio),'b-');
 axis([1 129 0 0.5]);set(gca,'ytick',0:0.1:0.5);
